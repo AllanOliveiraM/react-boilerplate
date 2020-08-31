1d72e2732cf889c11dd7aed9c8c320f433db1e9f
AllanOliveiraM <nallamo2016@gmail.com>
2020-08-31 08:06:49 -0300

    refactor(dashboard): remove unused component and route
    Removed unused route: Dashboard, never used in this project

--- src/AuthenticatedApp.js
+++ src/AuthenticatedApp.js
@@ -2,12 +2,10 @@ import React from 'react'
 import { Route, Redirect, Switch } from 'react-router-dom'
 
 import Home from 'routes/Home'
-import Dashboard from 'routes/Dashboard'
 
 const AuthenticatedApp = () => (
   <Switch>
     <Route path='/home' component={Home} />
-    <Route path='/dashboard' component={Dashboard} />
     <Redirect to='/home' />
   </Switch>
 )
